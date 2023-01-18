4b2425232fcf44409ed086f8d86f28dbd90b4c59
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-01-18 17:17:54 +0000

    spes: Build sensors@2.0-ScopedWakelock vendor variant
    Change-Id: I43eaca664ad9f0b41cffb4fc4805478dbcfed935
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -444,6 +444,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
