82974180c77fdadcb3c208558213e6d0fdf68f9d
muralivijay <muralidharan9845@gmail.com>
2023-09-29 17:27:41 +0000

    spes: Update DC dimming node.
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- parts/src/org/lineageos/settings/display/DisplayNodes.java
+++ parts/src/org/lineageos/settings/display/DisplayNodes.java
@@ -19,7 +19,7 @@ package org.lineageos.settings.display;
 public class DisplayNodes {
 
     private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
-    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display/msm_fb_ea_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/msm_fb_ea_enable";
     private static final String HBM_ENABLE_KEY = "hbm_mode";
     private static final String HBM_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
 
