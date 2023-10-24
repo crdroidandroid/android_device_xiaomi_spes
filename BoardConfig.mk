d1f5dad5af949d48011d4364e4a6122299b4ef59
Bruno Martins <bgcngm@gmail.com>
2023-10-24 18:12:15 +0100

    spes: BoardConfig: Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
      device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    Change-Id: I4ed1a06af779909d64172da294c4a99a3ae9ba0d
    Signed-off-by: ap4ss3rby <100168186+ap4ss3rby@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -247,7 +247,7 @@ BOARD_VNDK_VERSION := current
 NEED_AIDL_NDK_PLATFORM_BACKEND := true
 
 # Sepolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 ifdef CR_VERSION
