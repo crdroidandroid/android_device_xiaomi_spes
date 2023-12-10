7078572f70090d5a8241c2c6eb0cb1897481439b
ap4ss3rby <100168186+ap4ss3rby@users.noreply.github.com>
2023-12-10 18:25:26 +0200

    spes: Use libprotobuf-3.9.1

--- device.mk
+++ device.mk
@@ -416,7 +416,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
-    libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl \
     libxml2
 
