627b53b35367055c23fe14eb86bf023595a70eb7
muralivijay <muralidharan9845@gmail.com>
2024-01-25 17:51:00 +0530

    spes: Don't override 6GB dalvik vm props.
    * Our libinit doing this job . so no need copy this from framework.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- device.mk
+++ device.mk
@@ -10,9 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Enable Virtual A/B
 ifneq ($(WITH_GMS),true)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
