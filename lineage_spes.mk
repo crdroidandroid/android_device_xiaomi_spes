cffa5a165b3d5ecf56cbaf442b8b369bf311afce
Jingexz01 <jingexz01@gmail.com>
2025-08-20 08:16:13 +0800

    spes/spesn: Cleaned up lineage_spes.mk

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -34,6 +34,3 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Fingerprint
 BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V816.0.8.0.TGCMIXM:user/release-keys"
-
-# ADB Debug
-# WITH_ADB_INSECURE := true
